d6f4e6e9df136a868ea2ed6a42ff2fe9a49b97b4
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2022-02-08 16:24:31 +0200

    Latest bench

--- src/lib.rs
+++ src/lib.rs
@@ -17,3 +17,23 @@ pub async fn get_users() -> tide::http::Result<Body> {
     }
     Body::from_json(&users)
 }
+
+pub async fn get_users_spawn_blocking() -> tide::http::Result<Body> {
+    let users = async_std::task::spawn_blocking(|| {
+        let mut users = Vec::with_capacity(1000);
+        for index in 1..1001_u16 {
+            users.push(User {
+                Id: index,
+                Age: 25,
+                First_Name: format!("First_Name{}", index),
+                Last_Name: format!("Last_Name{}", index),
+                Framework: "Rust (tide)".to_owned(),
+            })
+        }
+
+        users
+    })
+    .await;
+
+    Body::from_json(&users)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use api_tide::get_users;
+use api_tide::{get_users, get_users_spawn_blocking};
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
     let mut app = tide::new();
     app.at("/users")
         .get(|_| async { Ok(get_users().await.unwrap()) });
+    // .get(|_| async { Ok(get_users_spawn_blocking().await.unwrap()) });
     app.listen("127.0.0.1:8084").await?;
     Ok(())
 }
